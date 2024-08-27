f7aab1a83ba088d344cc135b3f868d9900bd53dc
jasper <whiteana001@warren.k12.in.us>
2024-08-27 13:43:03 -0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(printEqual(1, 1, 1));
+        System.out.println(printEqual(1, 1, 2));
+        System.out.println(printEqual(-1, -1, -1));
+        System.out.println(printEqual(1, 2, 3));
+    }
+
+    private static String printEqual(int num1, int num2, int num3) {
+        if(num1 < 0 || num2 < 0 || num3 < 0){
+            return "Invalid";
+        }
+
+        if(num1 == num2 && num2 == num3){
+            return "All Equal";
+        }
+
+        if(num1 != num2 && num1 != num3){
+            return "All different";
+        }
+
+        else{
+            return "Neither";
+        }
+    }
 }
